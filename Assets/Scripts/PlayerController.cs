3e8619def91f945c7651a2dcfb4e5e9a7923d01f
nurmareko <nurmareko@yandex.ru>
2026-04-25 22:50:19 +0700

    Implement walking animation to player object

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,21 +2,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Rigidbody2D rigidBody;
+    [SerializeField] private Animator animator;
     [SerializeField] private float moveSpeed = 2.5f;
     [SerializeField] public Vector2 playerMoveDirection;
 
     // Update is called once per frame
     void Update()
     {
+        // Capture Keyboard input for movement
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
         playerMoveDirection = new Vector2(inputX, inputY).normalized;
+
+        // Trigger animation state
+        animator.SetFloat("Move X", inputX);
+        animator.SetFloat("Move Y", inputY);
+        animator.SetBool("Is Moving", playerMoveDirection != Vector2.zero);
+
     }
 
     void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(
+        rigidBody.linearVelocity = new Vector2(
             playerMoveDirection.x * moveSpeed,
             playerMoveDirection.y * moveSpeed
             );
