7d920be7bd5f16f8c795380d090d3884b1bd39b1
nurmareko <nurmareko@yandex.ru>
2026-05-02 18:15:58 +0700

    Implement enemy following player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,9 @@ public class Enemy : MonoBehaviour
 {
 
     [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Rigidbody2D rigidBody;
+    [SerializeField] private float moveSpeed;
+    private Vector2 direction;
 
     // Update is called once per frame
     void Update()
@@ -12,7 +15,17 @@ public class Enemy : MonoBehaviour
         float playerX = PlayerController.Instance.transform.position.x;
         float enemyX = gameObject.transform.position.x;
         bool isPlayerOnRight = playerX > enemyX;
-
         spriteRenderer.flipX = isPlayerOnRight;
+
+        Vector2 playerPosn = PlayerController.Instance.transform.position;
+        direction = (playerPosn - (Vector2) transform.position).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rigidBody.linearVelocity = new Vector2(
+            direction.x * moveSpeed,
+            direction.y * moveSpeed
+            );
     }
 }
